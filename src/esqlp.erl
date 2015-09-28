7b07a6a51b5dc3181139eb4a53edd45c3ef3293a
Sergej Jurecko <sergej.jurecko@wpn.tv>
2015-09-28 15:09:09 +0200

    select from

--- src/esqlp.erl
+++ src/esqlp.erl
@@ -25,6 +25,7 @@
 -define(X(X),(X == $x orelse X == $X)).
 -define(Y(Y),(Y == $y orelse Y == $Y)).
 -define(SKIP(X),(X == $\s orelse X == $\n  orelse X == $\r)).
+-define(WORD(X),(list_to_binary(lists:reverse(X)))).
 
 % State
 -record(dp,{cmd}).
@@ -77,8 +78,16 @@ cmd(P,<<U,P,D,A,T,E,SKIP,Bin/binary>>) when ?SKIP(SKIP) andalso ?U(U) andalso ?P
 cmd(P,<<D,E,L,E,T,E,SKIP,Bin/binary>>) when ?SKIP(SKIP) andalso ?D(D) andalso ?E(E) andalso ?L(L) andalso ?T(T)  ->
 	delete(P,Bin).
 
-% First check for modifier (distinct/all)
--record(sct,{modifier = all, cols = []}).
+% Based on diagrams at: http://sqlite.org/lang_select.html
+-record(from,{db, table, as}).
+-record(sct,{
+% distinct or all
+modifier = all, 
+% [ColumnNameBin1,ColumnNameBin2,...]
+cols = [], 
+% #from{}
+from = []}).
+
 select(P,<<C,Bin/binary>>) when ?SKIP(C) ->
 	select(P#dp{cmd = #sct{}},Bin);
 select(P,<<"all",C,Bin/binary>>) when ?SKIP(C) ->
@@ -106,20 +115,73 @@ select_cols(P,<<C,B/binary>>,[]) when ?SKIP(C) ->
 	select_cols(P,B,[]);
 select_cols(P,<<C,B/binary>>,Word) when ?SKIP(C) orelse C == $, ->
 	S = P#dp.cmd,
-	select_cols(P#dp{cmd = S#sct{cols = [list_to_binary(lists:reverse(Word))|S#sct.cols]}},B,[]);
+	select_cols(P#dp{cmd = S#sct{cols = [?WORD(Word)|S#sct.cols]}},B,[]);
 select_cols(P,<<"from",C,B/binary>>,[]) when ?SKIP(C) ->
-	select_from(P,B);
+	select_from(P,B,{[],[]});
 select_cols(P,<<"FROM",C,B/binary>>,[]) when ?SKIP(C) ->
-	select_from(P,B);
+	select_from(P,B,{[],[]});
 select_cols(P,<<"From",C,B/binary>>,[]) when ?SKIP(C) ->
-	select_from(P,B);
+	select_from(P,B,{[],[]});
 select_cols(P,<<F,R,O,M,C,B/binary>>,[]) when ?SKIP(C) andalso ?F(F) andalso ?R(R) andalso ?O(O) andalso ?M(M) ->
-	select_from(P,B);
+	select_from(P,B,{[],[]});
 select_cols(P,<<C,B/binary>>,L) ->
 	select_cols(P,B,[C|L]).
 
-select_from(P,<<C,Bin/binary>>) when ?SKIP(C)  ->
-	select_from(P,Bin).
+select_from(P,<<C,Bin/binary>>,{[],[]}) when ?SKIP(C)  ->
+	select_from(P,Bin,{[],[]});
+select_from(P,<<C,Bin/binary>>,[]) when ?SKIP(C)  ->
+	select_from(P,Bin,[]);
+select_from(P,<<C,B/binary>>,{_,_} = Word) when ?SKIP(C) ->
+	S = P#dp.cmd,
+	case Word of
+		{[_|_] = Table,[]} ->
+			select_from(P#dp{cmd = S#sct{from = [#from{table = ?WORD(Table)}|S#sct.from]}},B,{[],[]});
+		{Db,Table} ->
+			select_from(P#dp{cmd = S#sct{from = [#from{db = Db, table = ?WORD(Table)}|S#sct.from]}},B,{[],[]})
+	end;
+select_from(P,<<C,B/binary>>,[_|_] = Word) when ?SKIP(C) ->
+	% Finished AS
+	S = P#dp.cmd,
+	[H|Tl] = S#sct.from,
+	select_from(P#dp{cmd = S#sct{from = [H#from{as = ?WORD(Word)}|Tl]}},B,{[],[]});
+select_from(P,<<".",B/binary>>,{[_|_] = Word,[]}) ->
+	select_from(P,B,{?WORD(Word),[]});
+select_from(P,<<"as",C,B/binary>>,_) when ?SKIP(C) ->
+	select_from(P,B,[]);
+select_from(P,<<"AS",C,B/binary>>,_) when ?SKIP(C) ->
+	select_from(P,B,[]);
+select_from(P,<<"As",C,B/binary>>,_) when ?SKIP(C) ->
+	select_from(P,B,[]);
+select_from(P,<<"aS",C,B/binary>>,_) when ?SKIP(C) ->
+	select_from(P,B,[]);
+select_from(P,<<"where",C,B/binary>>,_) when ?SKIP(C) ->
+	select_where(P,B,[]);
+select_from(P,<<"Where",C,B/binary>>,_) when ?SKIP(C) ->
+	select_where(P,B,[]);
+select_from(P,<<"WHERE",C,B/binary>>,_) when ?SKIP(C) ->
+	select_where(P,B,[]);
+select_from(P,<<"group",C,B/binary>>,_) when ?SKIP(C) ->
+	select_group(P,B,[]);
+select_from(P,<<"Group",C,B/binary>>,_) when ?SKIP(C) ->
+	select_group(P,B,[]);
+select_from(P,<<"GROUP",C,B/binary>>,_) when ?SKIP(C) ->
+	select_group(P,B,[]);
+select_from(P,<<W,H,E,R,E,C,B/binary>>,_) when ?SKIP(C) andalso ?W(W) andalso ?H(H) andalso ?E(E) andalso ?R(R) ->
+	select_where(P,B,[]);
+select_from(P,<<G,R,O,U,P,C,B/binary>>,_) when ?SKIP(C) andalso ?G(G) andalso ?R(R) andalso ?O(O) andalso ?U(U) andalso ?P(P) ->
+	select_group(P,B,[]);
+select_from(P,<<C,B/binary>>,W) when is_list(W) ->
+	select_from(P,B,[C|W]);
+select_from(P,<<C,B/binary>>,{Db,[]}) when is_list(Db) ->
+	select_from(P,B,{[C|Db],[]});
+select_from(P,<<C,B/binary>>,{Db,Table}) when is_binary(Db), is_list(Table) ->
+	select_from(P,B,{Db,[C|Table]}).
+
+select_where(P,<<C,B/binary>>,[]) when ?SKIP(C) ->
+	select_where(P,B,[]).
+
+select_group(P,<<C,B/binary>>,[]) when ?SKIP(C) ->
+	select_group(P,B,[]).
 
 -record(ins,{}).
 insert(P,<<C,Bin/binary>>) when ?SKIP(C) ->
