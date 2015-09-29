df214cf777a964287143d8b628759396a614769b
Sergej Jurečko <sergej.jurecko@wpn.tv>
2015-09-29 21:03:28 +0200

    down the rabbit hole

--- src/esqlp.erl
+++ src/esqlp.erl
@@ -1,5 +1,7 @@
 -module(esqlp).
 -export([parse/1]).
+-include("esqlp.hrl").
+
 % -compile(export_all).
 -define(A(A),(A == $a orelse A == $A)).
 -define(B(B),(B == $b orelse B == $B)).
@@ -24,16 +26,14 @@
 -define(W(W),(W == $w orelse W == $W)).
 -define(X(X),(X == $x orelse X == $X)).
 -define(Y(Y),(Y == $y orelse Y == $Y)).
--define(SKIP(X),(X == $\s orelse X == $\n  orelse X == $\r)).
+-define(SKIP(X),(X == $\s orelse X == $\n  orelse X == $\t)).
 -define(WORD(X),(list_to_binary(lists:reverse(X)))).
 
-% State
--record(dp,{cmd}).
 
 parse(L) when is_list(L) ->
 	parse(iolist_to_binary(L));
 parse(Bin) ->
-	cmd(#dp{},Bin).
+	cmd(#sql{},Bin).
 
 cmd(P,<<C,B/binary>>) when ?SKIP(C) ->
 	cmd(P,B);
@@ -78,18 +78,10 @@ cmd(P,<<U,P,D,A,T,E,SKIP,Bin/binary>>) when ?SKIP(SKIP) andalso ?U(U) andalso ?P
 cmd(P,<<D,E,L,E,T,E,SKIP,Bin/binary>>) when ?SKIP(SKIP) andalso ?D(D) andalso ?E(E) andalso ?L(L) andalso ?T(T)  ->
 	delete(P,Bin).
 
-% Based on diagrams at: http://sqlite.org/lang_select.html
--record(from,{db, table, as}).
--record(sct,{
-% distinct or all
-modifier = all, 
-% [ColumnNameBin1,ColumnNameBin2,...]
-cols = [], 
-% #from{}
-from = []}).
+
 
 select(P,<<C,Bin/binary>>) when ?SKIP(C) ->
-	select(P#dp{cmd = #sct{}},Bin);
+	select(P#sql{cmd = #select{}},Bin);
 select(P,<<"all",C,Bin/binary>>) when ?SKIP(C) ->
 	select(P,Bin);
 select(P,<<"ALL",C,Bin/binary>>) when ?SKIP(C) ->
@@ -106,7 +98,7 @@ select(P,<<A,L,L1,C,Bin/binary>>) when ?SKIP(C) andalso ?A(A) andalso ?L(L) anda
 	select(P,Bin);
 select(P,<<D,I,S,T,I,N,C,T,SK,Bin/binary>>) when ?SKIP(SK) andalso ?D(D) andalso ?I(I) andalso ?S(S) andalso
 	?T(T) andalso ?N(N) andalso ?C(C) ->
-	select(P#dp{cmd = (P#dp.cmd)#sct{modifier = distinct}},Bin);
+	select(P#sql{cmd = (P#sql.cmd)#select{modifier = distinct}},Bin);
 select(P,Bin) ->
 	select_cols(P,Bin,[]).
 
@@ -114,8 +106,8 @@ select(P,Bin) ->
 select_cols(P,<<C,B/binary>>,[]) when ?SKIP(C) ->
 	select_cols(P,B,[]);
 select_cols(P,<<C,B/binary>>,Word) when ?SKIP(C) orelse C == $, ->
-	S = P#dp.cmd,
-	select_cols(P#dp{cmd = S#sct{cols = [?WORD(Word)|S#sct.cols]}},B,[]);
+	S = P#sql.cmd,
+	select_cols(P#sql{cmd = S#select{cols = [?WORD(Word)|S#select.cols]}},B,[]);
 select_cols(P,<<"from",C,B/binary>>,[]) when ?SKIP(C) ->
 	select_from(P,B,{[],[]});
 select_cols(P,<<"FROM",C,B/binary>>,[]) when ?SKIP(C) ->
@@ -132,18 +124,20 @@ select_from(P,<<C,Bin/binary>>,{[],[]}) when ?SKIP(C)  ->
 select_from(P,<<C,Bin/binary>>,[]) when ?SKIP(C)  ->
 	select_from(P,Bin,[]);
 select_from(P,<<C,B/binary>>,{_,_} = Word) when ?SKIP(C) ->
-	S = P#dp.cmd,
+	S = P#sql.cmd,
 	case Word of
 		{[_|_] = Table,[]} ->
-			select_from(P#dp{cmd = S#sct{from = [#from{table = ?WORD(Table)}|S#sct.from]}},B,{[],[]});
+			select_token(P#sql{curop = select_from,
+				cmd = S#select{from = [#from{table = ?WORD(Table)}|S#select.from]}},B);
 		{Db,Table} ->
-			select_from(P#dp{cmd = S#sct{from = [#from{db = Db, table = ?WORD(Table)}|S#sct.from]}},B,{[],[]})
+			select_token(P#sql{curop = select_from,
+				cmd = S#select{from = [#from{db = Db, table = ?WORD(Table)}|S#select.from]}},B)
 	end;
 select_from(P,<<C,B/binary>>,[_|_] = Word) when ?SKIP(C) ->
 	% Finished AS
-	S = P#dp.cmd,
-	[H|Tl] = S#sct.from,
-	select_from(P#dp{cmd = S#sct{from = [H#from{as = ?WORD(Word)}|Tl]}},B,{[],[]});
+	S = P#sql.cmd,
+	[H|Tl] = S#select.from,
+	select_token(P#sql{curop = select_from, cmd = S#select{from = [H#from{as = ?WORD(Word)}|Tl]}},B);
 select_from(P,<<".",B/binary>>,{[_|_] = Word,[]}) ->
 	select_from(P,B,{?WORD(Word),[]});
 select_from(P,<<"as",C,B/binary>>,_) when ?SKIP(C) ->
@@ -154,22 +148,6 @@ select_from(P,<<"As",C,B/binary>>,_) when ?SKIP(C) ->
 	select_from(P,B,[]);
 select_from(P,<<"aS",C,B/binary>>,_) when ?SKIP(C) ->
 	select_from(P,B,[]);
-select_from(P,<<"where",C,B/binary>>,_) when ?SKIP(C) ->
-	select_where(P,B,[]);
-select_from(P,<<"Where",C,B/binary>>,_) when ?SKIP(C) ->
-	select_where(P,B,[]);
-select_from(P,<<"WHERE",C,B/binary>>,_) when ?SKIP(C) ->
-	select_where(P,B,[]);
-select_from(P,<<"group",C,B/binary>>,_) when ?SKIP(C) ->
-	select_group(P,B,[]);
-select_from(P,<<"Group",C,B/binary>>,_) when ?SKIP(C) ->
-	select_group(P,B,[]);
-select_from(P,<<"GROUP",C,B/binary>>,_) when ?SKIP(C) ->
-	select_group(P,B,[]);
-select_from(P,<<W,H,E,R,E,C,B/binary>>,_) when ?SKIP(C) andalso ?W(W) andalso ?H(H) andalso ?E(E) andalso ?R(R) ->
-	select_where(P,B,[]);
-select_from(P,<<G,R,O,U,P,C,B/binary>>,_) when ?SKIP(C) andalso ?G(G) andalso ?R(R) andalso ?O(O) andalso ?U(U) andalso ?P(P) ->
-	select_group(P,B,[]);
 select_from(P,<<C,B/binary>>,W) when is_list(W) ->
 	select_from(P,B,[C|W]);
 select_from(P,<<C,B/binary>>,{Db,[]}) when is_list(Db) ->
@@ -177,8 +155,109 @@ select_from(P,<<C,B/binary>>,{Db,[]}) when is_list(Db) ->
 select_from(P,<<C,B/binary>>,{Db,Table}) when is_binary(Db), is_list(Table) ->
 	select_from(P,B,{Db,[C|Table]}).
 
-select_where(P,<<C,B/binary>>,[]) when ?SKIP(C) ->
-	select_where(P,B,[]).
+select_token(P,<<"where",C,B/binary>>) when ?SKIP(C) ->
+	expr_token(P,B);
+select_token(P,<<"Where",C,B/binary>>) when ?SKIP(C) ->
+	expr_token(P,B);
+select_token(P,<<"WHERE",C,B/binary>>) when ?SKIP(C) ->
+	expr_token(P,B);
+select_token(P,<<"group",C,B/binary>>) when ?SKIP(C) ->
+	select_group(P,B,[]);
+select_token(P,<<"Group",C,B/binary>>) when ?SKIP(C) ->
+	select_group(P,B,[]);
+select_token(P,<<"GROUP",C,B/binary>>) when ?SKIP(C) ->
+	select_group(P,B,[]);
+select_token(P,<<W,H,E,R,E,C,B/binary>>) when ?SKIP(C) andalso ?W(W) andalso ?H(H) andalso ?E(E) andalso ?R(R) ->
+	expr_token(P,B);
+select_token(P,<<G,R,O,U,P,C,B/binary>>) when ?SKIP(C) andalso ?G(G) andalso ?R(R) andalso ?O(O) andalso ?U(U) andalso ?P(P) ->
+	select_group(P,B,[]);
+select_token(#sql{curop = select_from} = P,B) ->
+	select_from(P,B,{[],[]});
+select_token(#sql{curop = select_where} = P,B) ->
+	expr_token(P,B);
+select_token(#sql{curop = select_group} = P,B) ->
+	select_group(P,B,[]).
+
+% select_where(P,<<C,B/binary>>) when ?SKIP(C) ->
+% 	select_where(P,B);
+% select_where(P,B) ->
+% 	expr_token(P,B).
+expr_token(P,<<"in",C,B/binary>>) when ?SKIP(C) ->
+	expr_in(P,B);
+expr_token(P,<<"In",C,B/binary>>) when ?SKIP(C) ->
+	expr_in(P,B);
+expr_token(P,<<"iN",C,B/binary>>) when ?SKIP(C) ->
+	expr_in(P,B);
+expr_token(P,<<"IN",C,B/binary>>) when ?SKIP(C) ->
+	expr_in(P,B);
+expr_token(P,<<"not",C,B/binary>>) when ?SKIP(C) ->
+	expr_not(P,B);
+expr_token(P,<<"NOT",C,B/binary>>) when ?SKIP(C) ->
+	expr_not(P,B);
+expr_token(P,<<"Not",C,B/binary>>) when ?SKIP(C) ->
+	expr_not(P,B);
+expr_token(P,<<"nOt",C,B/binary>>) when ?SKIP(C) ->
+	expr_not(P,B);
+expr_token(P,<<"noT",C,B/binary>>) when ?SKIP(C) ->
+	expr_not(P,B);
+expr_token(P,<<"like",C,B/binary>>) when ?SKIP(C) ->
+	expr_like(P,B);
+expr_token(P,<<"Like",C,B/binary>>) when ?SKIP(C) ->
+	expr_like(P,B);
+expr_token(P,<<"LIKE",C,B/binary>>) when ?SKIP(C) ->
+	expr_like(P,B);
+expr_token(P,<<L,I,K,E,C,B/binary>>) when ?SKIP(C) andalso ?L(L) andalso ?I(I) andalso ?K(K) andalso ?E(E) ->
+	expr_like(P,B);
+expr_token(P,B) ->
+	expr_literal(P,B).
+
+expr_in(P,<<C,B/binary>>) when ?SKIP(C) ->
+	expr_in(P,B).
+
+expr_like(P,<<C,B/binary>>) when ?SKIP(C) ->
+	expr_like(P,B).
+
+expr_literal(P,<<C,B/binary>>) when ?SKIP(C) ->
+	expr_literal(P,B).
+
+expr_not(P,<<C,B/binary>>) when ?SKIP(C) ->
+	expr_not(P,B);
+expr_not(P,<<"null",C,B/binary>>) when ?SKIP(C) ->
+	expr_not_null(P,B);
+expr_not(P,<<"Null",C,B/binary>>) when ?SKIP(C) ->
+	expr_not_null(P,B);
+expr_not(P,<<"NULL",C,B/binary>>) when ?SKIP(C) ->
+	expr_not_null(P,B);
+expr_not(P,<<"like",C,B/binary>>) when ?SKIP(C) ->
+	expr_like(P,B);
+expr_not(P,<<"Like",C,B/binary>>) when ?SKIP(C) ->
+	expr_not_like(P,B);
+expr_not(P,<<"LIKE",C,B/binary>>) when ?SKIP(C) ->
+	expr_not_like(P,B);
+expr_not(P,<<"in",C,B/binary>>) when ?SKIP(C) ->
+	expr_not_in(P,B);
+expr_not(P,<<"In",C,B/binary>>) when ?SKIP(C) ->
+	expr_not_in(P,B);
+expr_not(P,<<"iN",C,B/binary>>) when ?SKIP(C) ->
+	expr_not_in(P,B);
+expr_not(P,<<"IN",C,B/binary>>) when ?SKIP(C) ->
+	expr_not_in(P,B);
+expr_not(P,<<N,U,L,L,C,B/binary>>) when ?SKIP(C) andalso ?N(N) andalso ?U(U) andalso ?L(L) ->
+	expr_not_null(P,B);
+expr_not(P,<<L,I,K,E,C,B/binary>>) when ?SKIP(C) andalso ?L(L) andalso ?I(I) andalso ?K(K) andalso ?E(E) ->
+	expr_not_like(P,B).
+
+expr_list_literal(P,<<C,B/binary>>) when ?SKIP(C) ->
+	expr_list_literal(P,B).
+
+expr_not_in(P,B) ->
+	expr_list_literal(P#sql{operator = [notin|P#sql.operator]},B).
+
+expr_not_like(P,B) ->
+	expr_literal(P#sql{operator = [notlike|P#sql.operator]},B).
+
+expr_not_null(P,B) ->
+	expr_token(P#sql{operator = [{notnull,hd(P#sql.literal)}, literal = tl(P#sql.literal)]},B).
 
 select_group(P,<<C,B/binary>>,[]) when ?SKIP(C) ->
 	select_group(P,B,[]).
